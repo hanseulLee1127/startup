74ef9e2204d99cabfb7624947b9ed199089b8d3c
HanseulLee1127 <leehs004@byu.edu>
2023-12-11 23:32:54 -0700

    finished working on finding_parking.js , html

--- finding_parking.js
+++ finding_parking.js
@@ -31,7 +31,6 @@ class finding_parking {
         this.loggedInUser = this.getUsername();
     }
 
-
     getUser() {
         const userAccountsString = localStorage.getItem('userAccounts');
         
