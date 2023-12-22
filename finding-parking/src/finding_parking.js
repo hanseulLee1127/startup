6c12f8f67fffcbe833dfc6c828d5269dbb7a0ef1
HanseulLee1127 <leehs004@byu.edu>
2023-12-21 17:12:39 -0700

    convert to react.

--- finding-parking/src/finding_parking.js
+++ finding-parking/src/finding_parking.js
@@ -3,10 +3,19 @@ import './styles.css';
 
 function FindingParking() {
   const [username, setUsername] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [buildings, setBuildings] = useState([]);
+  const [parkingLots, setParkingLots] = useState([]);
+  const [selectedBuildingName, setSelectedBuildingName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   // other states like parkingLots, buildings, messages, etc.
 
   // useEffect to fetch user data, initialize WebSocket, etc.
-
+  useEffect(() => {
+    fetchJsonData('building.json', 'buildings').then(data => setBuildings(data));
+    fetchJsonData('parkingLot.json', 'lots').then(data => setParkingLots(data));
+    getUsername();
+  }, []);
   // Functions to handle search, save parking lot, WebSocket messages, etc.
 
   return (
